67e140143d3634703764a8eed90687a7d56fa8e7
Paul Veltheim <27195593+ankkalampi@users.noreply.github.com>
2025-03-19 14:05:56 +0200

    made some tweaks

--- A10/include/vs.hpp
+++ A10/include/vs.hpp
@@ -1,9 +1,8 @@
 /**
  * You can use this file for programmign task 1 of set 1.
-*/
+ */
 
 #pragma once
-
 
 #include <algorithm>
 #include <array>
@@ -12,34 +11,27 @@
 
 namespace pfp {
 
-template <class dtype>
-class vs {
-  public:
+template <class dtype> class vs {
+public:
   static const int numberVecs = 100000;
-  static constexpr int numbersInVec =  2147483647 / numberVecs;
-    std::vector<std::vector<dtype>> vecs;
-    
-    vs(){
-      vecs.resize(numberVecs);
-    }
- 
-    void insert(dtype val) { 
-      int index = val / numbersInVec;
-      if (index >= numberVecs) index = numberVecs -1;
-      vecs[index].push_back(val);
+  static constexpr int numbersInVec = 2147483647 / numberVecs;
+  std::vector<std::vector<dtype>> vecs;
 
-    }
+  vs() { vecs.resize(numberVecs); }
 
+  void insert(dtype val) {
+    int index = val / numbersInVec;
+    // if (index >= numberVecs) index = numberVecs -1;
+    vecs[index].push_back(val);
+  }
 
-    int count(dtype val) {
+  int count(dtype val) {
 
-      int index = val / numbersInVec;
-      std::sort(vecs[index].begin(), vecs[index].end());
-      return std::binary_search(vecs[index].begin(), vecs[index].end(), val) ? 1 : 0;
-
-    }
-
-   
+    int index = val / numbersInVec;
+    std::sort(vecs[index].begin(), vecs[index].end());
+    return std::binary_search(vecs[index].begin(), vecs[index].end(), val) ? 1
+                                                                           : 0;
+  }
 };
 
-}  // namespace pfp
+} // namespace pfp
